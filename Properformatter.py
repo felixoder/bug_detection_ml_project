2c14ea1fdb7ce133a1c9439ac6c072d7d663bfec
felixoder <debayanghosh408@gmail.com>
2025-04-06 20:10:28 +0530

    added a nice formatter for better view at github solve #2 issue

--- Properformatter.py
+++ Properformatter.py
@@ -13,4 +13,4 @@ if "widgets" in notebook.get("metadata", {}):
 with open("bug_detection_clean.ipynb", "w", encoding="utf-8") as f:
     json.dump(notebook, f, indent=1)
 
-# print("Saved cleaned notebook as 'usingModel_clean.ipynb'")
+print("modified the .ipynb file to be visited properly at github")
