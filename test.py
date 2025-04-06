617d0faffb6fb822e8ba363491855d1e412c3e7c
felixoder <debayanghosh408@gmail.com>
2025-04-06 20:03:29 +0530

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+import json
+
+# Load the notebook
+with open("usingModel.ipynb", "r", encoding="utf-8") as f:
+    notebook = json.load(f)
+
+# Remove the problematic widgets metadata
+if "widgets" in notebook.get("metadata", {}):
+    del notebook["metadata"]["widgets"]
+    print("Removed metadata['widgets']")
+
+# Save the cleaned notebook
+with open("usingModel_clean.ipynb", "w", encoding="utf-8") as f:
+    json.dump(notebook, f, indent=1)
+
+print("Saved cleaned notebook as 'usingModel_clean.ipynb'")
